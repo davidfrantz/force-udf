1ff97be8789fb744497f48492e73fd1a9d9467bb
David Frantz <d-frantz@web.de>
2021-03-15 09:03:44 +0100

    some formatting in medoid

--- python/ard/medoid/pixel_simple/medoid.py
+++ python/ard/medoid/pixel_simple/medoid.py
@@ -1,13 +1,36 @@
 import numpy as np
 from scipy.spatial.distance import squareform, pdist
 
+"""
+>>> Medoid
+>>> Copyright (C) 2021 Andreas Rabe
+"""
 
-def forcepy_init(dates, sensors, bandNames):
-    return bandNames
+def forcepy_init(dates, sensors, bandnames):
+    """
+    dates:     numpy.ndarray[nDates](int) days since epoch (1970-01-01)
+    sensors:   numpy.ndarray[nDates](str)
+    bandnames: numpy.ndarray[nBands](str)
+    """
+    
+    print(dates)
+    print(sensors)
+    print(bandnames)
+
+    return bandnames
 
 
 def forcepy_pixel(inarray, outarray, dates, sensors, bandnames, nodata, nproc):
-    """Calculate medoid."""
+    """
+    inarray:   numpy.ndarray[nDates, nBands, nrows, ncols](Int16)
+    outarray:  numpy.ndarray[nOutBands](Int16) initialized with no data values
+    dates:     numpy.ndarray[nDates](int) days since epoch (1970-01-01)
+    sensors:   numpy.ndarray[nDates](str)
+    bandnames: numpy.ndarray[nBands](str)
+    nodata:    int
+    nproc:     number of allowed processes/threads (always 1)
+    Write results into outarray.
+    """
 
     inarray = inarray[:, :, 0, 0]
     valid = np.where(inarray[:, 0] != nodata)[0]  # skip no data; just check first band
